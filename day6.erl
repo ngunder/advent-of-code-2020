daaa40b9d10a5927568adf463484313819640be1
Nicholas Gunder <ngu@tradeshift.com>
2020-12-05 22:40:36 -0800

    Day6

---
+++ day6.erl
@@ -0,0 +1,149 @@
+-module(day6).
+-export([run/0]).
+% --- Day 6: Custom Customs ---
+% As your flight approaches the regional airport where you'll switch to a much
+%  larger plane, customs declaration forms are distributed to the passengers.
+
+% The form asks a series of 26 yes-or-no questions marked a through z. All you 
+% need to do is identify the questions for which anyone in your group answers 
+% "yes". Since your group is just you, this doesn't take very long.
+
+% However, the person sitting next to you seems to be experiencing a language 
+% barrier and asks if you can help. For each of the people in their group, 
+% you write down the questions for which they answer "yes", one per line. For 
+% example:
+
+% abcx
+% abcy
+% abcz
+% In this group, there are 6 questions to which anyone answered "yes": a, b, 
+% c, x, y, and z. (Duplicate answers to the same question don't count extra; 
+% each question counts at most once.)
+
+% Another group asks for your help, then another, and eventually you've 
+% collected answers from every group on the plane (your puzzle input). Each 
+% group's answers are separated by a blank line, and within each group, each 
+% person's answers are on a single line. For example:
+
+% abc
+
+% a
+% b
+% c
+
+% ab
+% ac
+
+% a
+% a
+% a
+% a
+
+% b
+% This list represents answers from five groups:
+
+% The first group contains one person who answered "yes" to 3 questions: a, b, 
+% and c.
+% The second group contains three people; combined, they answered "yes" to 3 
+% questions: a, b, and c.
+% The third group contains two people; combined, they answered "yes" to 3 
+% questions: a, b, and c.
+% The fourth group contains four people; combined, they answered "yes" to 
+% only 1 question, a.
+% The last group contains one person who answered "yes" to only 1 question, b.
+% In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+
+% For each group, count the number of questions to which anyone answered 
+% "yes". What is the sum of those counts?
+
+% Your puzzle answer was 6416.
+
+% --- Part Two ---
+% As you finish the last group's customs declaration, you notice that you 
+% misread one word in the instructions:
+
+% You don't need to identify the questions to which anyone answered "yes"; 
+% you need to identify the questions to which everyone answered "yes"!
+
+% Using the same example as above:
+
+% abc
+
+% a
+% b
+% c
+
+% ab
+% ac
+
+% a
+% a
+% a
+% a
+
+% b
+% This list represents answers from five groups:
+
+% In the first group, everyone (all 1 person) answered 
+% "yes" to 3 questions: a, b, and c.
+% In the second group, there is no question to which 
+% everyone answered "yes".
+% In the third group, everyone answered yes to only 
+% 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+% In the fourth group, everyone answered yes to only 
+% 1 question, a.
+% In the fifth group, everyone (all 1 person) answered 
+% "yes" to 1 question, b.
+% In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+% For each group, count the number of questions to which everyone answered 
+% "yes". What is the sum of those counts?
+
+run() ->
+    Data = common:readlines("inputs/day6.txt"),
+    io:format("~p~n", [length(lists:flatten(get_group_answers(Data,1)))]),
+    io:format("~p~n", [length(lists:flatten(get_group_answers(Data,2)))]).
+
+
+get_group_answers(Data, Part) ->
+    get_group_answers({Data, Part, 0}, [], []).
+get_group_answers({[],_,_}, Answers, Acc) ->
+    [Answers,Acc];
+get_group_answers({[<<>>|T], Part, _}, Answers, Acc) ->
+    get_group_answers({T,Part,0}, [], [Answers|Acc]);
+get_group_answers({[H|T],1,Count}, Answers, Acc) ->
+    get_group_answers({T,1,Count}, find_unique(binary_to_list(H), Answers), Acc);
+get_group_answers({[H|T],2,Count}, Answers, Acc) ->
+    get_group_answers({T,2,Count+1}, find_matching(binary_to_list(H), Count, Answers), Acc).
+
+
+find_matching(L1, Count, L2) ->
+    find_matching(L1, Count, L2, []).
+find_matching([], _Count, _, Acc) ->
+    Acc;
+find_matching(List, 0, [], Acc) ->
+    List ++ Acc;
+find_matching([H1], _Count, [H2], Acc) when H1 =/= H2 ->
+    Acc;
+find_matching([H|T], Count, List2, Acc) ->
+    case lists:member(H, List2) of
+        true ->
+            find_matching(T, Count, List2, [H|Acc]);
+        false ->
+            find_matching(T, Count, List2, Acc)
+    end.
+
+find_unique([], List2) ->
+    List2;
+find_unique([H|T], List2) ->
+    find_unique(T, insert_unique(List2, H)).
+
+insert_unique(List, Elm) ->
+    insert_unique(List, Elm, []).
+insert_unique([], Elm, Acc) ->
+    [Elm|Acc];
+insert_unique([Elm|T], Elm, Acc) ->
+    T++[Elm|Acc];
+insert_unique([H|T], Elm, Acc) ->
+    insert_unique(T, Elm, [H|Acc]).
+
